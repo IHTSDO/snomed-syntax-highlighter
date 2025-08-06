18416a8faa9eb4a1b42e52d236b65ed5cdb87d1d
Kai Kewley <kaikewley@gmail.com>
2025-08-06 23:13:22 +0100

    Add buttons to adjust WebFrame size.

--- gitbook_integration/src/index.tsx
+++ gitbook_integration/src/index.tsx
@@ -6,9 +6,9 @@ import {
 } from "@gitbook/runtime";
 
 type IntegrationContext = {} & RuntimeContext;
-type IntegrationBlockProps = { content?: string };
-type IntegrationBlockState = { content?: string };
-type IntegrationAction = { action: 'click' } | { action: 'contentChange' };
+type IntegrationBlockProps = { content?: string, lines?: number };
+type IntegrationBlockState = { content?: string, lines?: number };
+type IntegrationAction = { action: 'click' } | { action: 'contentChange' } | { action: 'increaseSize' };
 
 const handleFetchEvent: FetchEventCallback<IntegrationContext> = async (
   request,
@@ -31,19 +31,58 @@ const exampleBlock = createComponent<
     console.log("props", props);
     if (props.content) {
       return {
+        lines: props.lines,
         content: props.content,
       };
     }
     return {
+      lines: 1,
       content: "404684003 |Clinical finding (finding)|",
     };
   },
   action: async (element, action, context) => {
-    return element;
-  },
+    switch (action.action) {
+      case "increaseSize":
+        console.log("Increase size", element);
+        console.log("Increase size lines", element.state.lines);
+        console.log("Increase size content", element.state.content);
+        let lines = element.state.lines || 1;
+        lines = lines + 1;
+        if (lines > 10) {
+          lines = 10;
+        }
+        return {
+          action: '@editor.node.updateProps',
+          props: {
+            lines: lines,
+            content: element.state.content
+          }
+        }
+      case "decreaseSize":
+        console.log("Decrease size", element);
+        console.log("Decrease size lines", element.state.lines);
+        console.log("Decrease size content", element.state.content);
+        let linesB = element.state.lines || 1;
+        linesB = linesB -1;
+        if (linesB < 1) {
+          linesB = 1;
+        }
+        return {
+          action: '@editor.node.updateProps',
+          props: {
+            lines: linesB,
+            content: element.state.content
+          }
+        }
+      default:
+        console.log("❌ Unknown action:", action.action);
+        return element;
+    }  },
   render: async (element, context) => {
     console.log("render ", JSON.stringify(element));
     const { content } = element.state;
+    let lines = element.state.lines;
+    console.log("render lines ", lines);
     const isEditable = element.context.editable;
     
     const expressionUrl = `https://ihtsdo.github.io/snomed-syntax-highlighter/?expression=${encodeURIComponent(content || '')}`;
@@ -58,6 +97,7 @@ const exampleBlock = createComponent<
               onContentChange={{ 
                 action: '@editor.node.updateProps',
                 props: {
+                  lines: lines,
                   content: element.dynamicState('content')
                 }
               }}
@@ -65,7 +105,11 @@ const exampleBlock = createComponent<
           <text>This is how it will look:</text>
           <webframe
             source={{ url: expressionUrl }}
+            aspectRatio={710/(23 + (20 * lines))}
             />
+          <text>Frame size: {lines + ''} lines</text>
+          <button label="Smaller" onPress={{ action: 'decreaseSize' }} />
+          <button label="Bigger" onPress={{ action: 'increaseSize' }} />
         </block>
       );  
     } else {
@@ -73,6 +117,7 @@ const exampleBlock = createComponent<
       <block>
         <webframe
           source={{ url: expressionUrl }}
+          aspectRatio={710/(23 + (20 * lines))}
           />
       </block>
     );
