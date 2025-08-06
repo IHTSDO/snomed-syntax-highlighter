4d9fa059cf015a360f2a59701560813df82af2a4
Kai Kewley <kaikewley@gmail.com>
2025-08-06 20:32:12 +0100

    Remove redundant code.

--- gitbook_integration/src/index.tsx
+++ gitbook_integration/src/index.tsx
@@ -35,33 +35,11 @@ const exampleBlock = createComponent<
       };
     }
     return {
-      content: "initial content 1",
+      content: "404684003 |Clinical finding (finding)|",
     };
   },
   action: async (element, action, context) => {
-    switch (action.action) {
-      case "click":
-        console.log("Button Clicked");
-        return element;
-      case "contentChange":
-        console.log("Content changed", JSON.stringify(element));
-        const newValue = element.state.content
-        console.log("New Value", newValue);
-        return {
-          ...element,
-          // The state is already updated automatically by the codeblock
-          // Just trigger the editor node update
-          action: {
-            action: '@editor.node.updateProps',
-            props: {
-              content: newValue // Use the current state content
-            }
-          }
-        };
-      default:
-        console.log("❌ Unknown action:", action.action);
-        return element;
-    }
+    return element;
   },
   render: async (element, context) => {
     console.log("render ", JSON.stringify(element));
@@ -76,7 +54,7 @@ const exampleBlock = createComponent<
           <text>You are in edit mode. Put the SNOMED ECL or Expression in this box.</text>
             <codeblock 
               state="content" 
-              content={content || "initial content"} 
+              content={content || ""} 
               onContentChange={{ 
                 action: '@editor.node.updateProps',
                 props: {
